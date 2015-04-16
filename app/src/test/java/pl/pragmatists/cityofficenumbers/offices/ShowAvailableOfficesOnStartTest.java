efaee9f4505f27d0e2ff85e9503cd4837897deb5
Krzysztof Jelski <krzysztof.jelski@pragmatists.pl>
2015-04-16 09:47:37 +0200

    I can haz an robo electric activity.

--- app/src/test/java/pl/pragmatists/cityofficenumbers/offices/ShowAvailableOfficesOnStartTest.java
+++ app/src/test/java/pl/pragmatists/cityofficenumbers/offices/ShowAvailableOfficesOnStartTest.java
@@ -3,11 +3,14 @@ package pl.pragmatists.cityofficenumbers.offices;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
-import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
 
+import pl.pragmatists.cityofficenumbers.app.BuildConfig;
 import pl.pragmatists.cityofficenumbers.app.SelectOffice;
 
-@RunWith(RobolectricTestRunner.class)
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, emulateSdk = 21)
 public class ShowAvailableOfficesOnStartTest {
 
     @Test
