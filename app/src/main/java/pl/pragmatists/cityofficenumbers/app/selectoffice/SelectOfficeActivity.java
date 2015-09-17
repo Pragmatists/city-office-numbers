db02e7c58d6f49eaf161dc60914e3dbfa7a38d74
Krzysztof Jelski <krzysztof.jelski@pragmatists.pl>
2015-09-17 17:52:38 +0200

    Menu bar styling.

--- app/src/main/java/pl/pragmatists/cityofficenumbers/app/selectoffice/SelectOfficeActivity.java
+++ app/src/main/java/pl/pragmatists/cityofficenumbers/app/selectoffice/SelectOfficeActivity.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -90,27 +88,6 @@ public class SelectOfficeActivity extends AppCompatActivity {
         return (CityOfficeNumbersApplication) getApplication();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_select_office, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     public void toggleFavorite(View v) {
         Office office = (Office) v.getTag();
