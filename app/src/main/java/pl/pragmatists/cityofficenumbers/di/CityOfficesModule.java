5af5ec5c034b42c2877f63a8b008e6be5650346a
Krzysztof Jelski <krzysztof.jelski@pragmatists.pl>
2016-02-19 12:26:40 +0100

    Extract host.

--- app/src/main/java/pl/pragmatists/cityofficenumbers/di/CityOfficesModule.java
+++ app/src/main/java/pl/pragmatists/cityofficenumbers/di/CityOfficesModule.java
@@ -2,11 +2,11 @@ package pl.pragmatists.cityofficenumbers.di;
 
 import dagger.Module;
 import dagger.Provides;
-import pl.pragmatists.cityofficenumbers.events.BusInstance;
 import pl.pragmatists.cityofficenumbers.app.selectoffice.CityOfficesFetcher;
 import pl.pragmatists.cityofficenumbers.app.selectoffice.CityOfficesHardcoded;
 import pl.pragmatists.cityofficenumbers.app.selectoffice.CityOfficesModel;
 import pl.pragmatists.cityofficenumbers.app.selectoffice.FavoriteService;
+import pl.pragmatists.cityofficenumbers.events.BusInstance;
 import pl.pragmatists.cityofficenumbers.events.EventBus;
 import pl.pragmatists.http.Host;
 import pl.pragmatists.http.RestClientWithOkHttp;
@@ -20,8 +20,13 @@ public class CityOfficesModule {
     }
 
     @Provides
-    CityOfficesFetcher officesFetcher(EventBus eventBus) {
-        return new CityOfficesFetcher(new RestClientWithOkHttp(new Host("http://10.0.2.2:8080")), eventBus);
+    CityOfficesFetcher officesFetcher(EventBus eventBus, Host host) {
+        return new CityOfficesFetcher(new RestClientWithOkHttp(host), eventBus);
+    }
+
+    @Provides
+    FavoriteService favoriteService(Host host) {
+        return new FavoriteService(new RestClientWithOkHttp(host));
     }
 
     @Provides
@@ -30,8 +35,8 @@ public class CityOfficesModule {
     }
 
     @Provides
-    FavoriteService favoriteService() {
-        return new FavoriteService(new RestClientWithOkHttp(new Host("http://10.0.2.2:8080")));
+    private Host host() {
+        return new Host("https://city-office-numbers.herokuapp.com");
     }
 
 }
